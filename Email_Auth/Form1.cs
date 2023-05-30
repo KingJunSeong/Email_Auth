7e765ae9143519163f95f32a6e18f3b84427069f
Leaf <rlawnstjddl12@gmail.com>
2023-05-30 19:45:43 +0900

    초기 커밋.

---
+++ Email_Auth/Form1.cs
@@ -0,0 +1,29 @@
+using System.Text.RegularExpressions;
+
+namespace Email_Auth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(IsVaildEmail(textBox1.Text) == true)
+            {
+                //성공
+            }
+            else
+            {
+                MessageBox.Show("올바른 이메일 형식이 아닙니다.");
+            }
+        }
+        private bool IsVaildEmail(string email)
+        {
+            bool valid = Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+            return valid;
+        }
+    }
+}
